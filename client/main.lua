6fefe93d01dfd9576ebe26c2e05acb19394d17a9
guillerp <72713057+guillerp8@users.noreply.github.com>
2021-03-10 13:49:11 +0100

    Optimzation in client-side
    I added variables in wait to change depending the situation, if the player is far from the marker the wait time will increase, and if is near will decrease.

--- client/main.lua
+++ client/main.lua
@@ -68,42 +68,48 @@ end)
 -- Display markers
 Citizen.CreateThread(function()
 	while true do
-		Wait(0)
+		local wait = 1200
 		local coords = GetEntityCoords(GetPlayerPed(-1))
 
 		for k,v in pairs(Config.Zones) do
 			if PlayerData ~= nil  then
 				if(v.Type ~= -1 and GetDistanceBetweenCoords(coords, v.Pos.x, v.Pos.y, v.Pos.z, true) < Config.DrawDistance) then
+						wait = 0
 					DrawMarker(v.Type, v.Pos.x, v.Pos.y, v.Pos.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, v.Size.x, v.Size.y, v.Size.z, v.Color.r, v.Color.g, v.Color.b, 100, false, true, 2, false, false, false, false)
 				end
 			end
 		end
+		Citizen.Wait(wait)
 	end
 end)
 -- Enter / Exit marker events
 Citizen.CreateThread(function()
 	while true do
-		Wait(0)
+		local wait = 120p
 		if PlayerData ~= nil  then
 			local coords      = GetEntityCoords(GetPlayerPed(-1))
 			local isInMarker  = false
 			local currentZone = nil
 			for k,v in pairs(Config.Zones) do
 				if(GetDistanceBetweenCoords(coords, v.Pos.x, v.Pos.y, v.Pos.z, true) < v.Size.x) then
+					wait = 0
 					isInMarker  = true
 					currentZone = k
 				end
 			end
 			if (isInMarker and not HasAlreadyEnteredMarker) or (isInMarker and LastZone ~= currentZone) then
+				wait = 0
 				HasAlreadyEnteredMarker = true
 				LastZone                = currentZone
 				TriggerEvent('esx_illegalWarehouses:hasEnteredMarker', currentZone)
 			end
 			if not isInMarker and HasAlreadyEnteredMarker then
+				wait = 0
 				HasAlreadyEnteredMarker = false
 				TriggerEvent('esx_illegalWarehouses:hasExitedMarker', LastZone)
 			end
 		end
+		Citizen.Waot(wait)
 	end
 end)
 
